0142337ae5d0dd670c36e4c84c5c0e464a7c1c29
lifanova <veralifanovaa@gmail.com>
2021-06-21 11:45:08 +0300

    User page actions are added

--- public/homePage.js
+++ public/homePage.js
@@ -1,9 +1,9 @@
 // Выход из личного кабинета
 let logoutButton = new LogoutButton();
-logoutButton.action = () => {    
+logoutButton.action = () => {
     //logout (callback)
-    ApiConnector.logout((response) => {        
-        if (response.success) {            
+    ApiConnector.logout((response) => {
+        if (response.success) {
             location.reload();
         }
     });
@@ -24,6 +24,7 @@ ApiConnector.current((response) => {
 let ratesBoard = new RatesBoard();
 let processStocks = () => {
     // получение курсов валют (последние 100 записей)
+    console.log("[processStocks]: ");
     ApiConnector.getStocks(stocksCallBack);
 };
 
@@ -35,10 +36,86 @@ let stocksCallBack = (response) => {
 }
 
 processStocks();
+// запускаем с интервалом 1 мин
 let timerId = setInterval(processStocks, 60000);
 
-//Операции с деньгами
-let moneyManager = new MoneyManager();
+//Операции с деньгами:
+// 1. Пополнить баланс
+// 2. Конвертировать валюту
+// 3. Перевод денег пользователю
 
-//Работа с избранным
+let moneyManager = new MoneyManager();
+// переопределяем callback
+moneyManager.addMoneyCallback = (moneyData) => {
+    // moneyData -> { currency, amount }
+    // запрос на пополнение баланса
+    ApiConnector.addMoney((response) => {
+        let msg = (response.success) ? "Баланс пополнен" : "Выполнить операцию не удалось";
+        moneyManager.setMessage(response.success, msg);
+
+        ProfileWidget.showProfile(response.data);        
+    })
+};
+
+moneyManager.conversionMoneyCallback = (moneyData) => {
+    // moneyData -> { fromCurrency, targetCurrency, fromAmount }
+    ApiConnector.convertMoney((moneyData, response) => {
+        let msg = (response.success) ? "Операция конвертации выполнена" : "Выполнить операцию не удалось";
+        moneyManager.setMessage(response.success, msg);
+
+        ProfileWidget.showProfile(response.data);        
+    });
+}
+
+moneyManager.sendMoneyCallback = (moneyData) => {
+    // moneyData -> { to, amount, currency }
+    ApiConnector.transferMoney((moneyData, response) => {
+        let msg = (response.success) ? "Операция перевода выполнена" : "Выполнить операцию не удалось";
+        moneyManager.setMessage(response.success, msg);
+
+        ProfileWidget.showProfile(response.data);        
+    });
+}
+
+
+//Работа с избранным:
+// 1. Получить список
+// 2. Добавить нового пользователя в избранное
+// 3. Удалить нового пользователя из избранного
+
 let favoritesWidget = new FavoritesWidget();
+//начальный список избранного
+let getInitialFavorites = () => {
+    ApiConnector.getFavorites((response) => {
+        if (response.success) {
+            newFavoritesWidget.clearTable();
+            newFavoritesWidget.fillTable(response.data);
+            newMoneyManager.updateUsersList(response.data);
+        }
+    });
+};
+
+//вызываем
+getInitialFavorites();
+
+favoritesWidget.addUserCallback = (data) => {
+    ApiConnector.addUserToFavorites((data, response) => {
+        let msg = (response.success) ? "Операция добавления нового пользователя выполнена" : "Выполнить операцию не удалось";
+        favoritesWidget.setMessage(response.success, msg);
+
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);        
+    });
+}
+
+favoritesWidget.removeUserCallback = (id) => {
+    ApiConnector.removeUserFromFavorites(id, response => {
+        let msg = (response.success) ? "Операция удаления пользователя выполнена" : "Выполнить операцию не удалось";
+        favoritesWidget.setMessage(response.success, msg);          
+            
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);        
+    });
+}
